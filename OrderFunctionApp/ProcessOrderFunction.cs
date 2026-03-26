4685f87654292b22c6f3853f96ae7a952b155f7d
NikhilHEHE <sharmanikhil340@gmail.com>
2026-03-27 00:35:26 +0530

    Added code for logic app

--- OrderFunctionApp/ProcessOrderFunction.cs
+++ OrderFunctionApp/ProcessOrderFunction.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Logging;
 using OrderFunctionApp.Data;
 using System;
 using OrderFunctionApp.Models;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 
 namespace OrderFunctionApp
 {
@@ -39,10 +42,27 @@ namespace OrderFunctionApp
             if (order != null)
             {
                 order.Status = OrderStatus.Completed;
-
                 await context.SaveChangesAsync();
 
                 _logger.LogInformation($"Order {orderId} marked as Completed");
+
+                var logicAppUrl = "https://prod-02.centralindia.logic.azure.com:443/workflows/5c34a2145a504a9d9fd6d46293b1a65e/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=hOW8G0aXT-4721H2Qt27ue7fTa-UL7mRg4oeHaR3oEo";
+
+                var payload = new
+                {
+                    orderId = order.Id,
+                    email = order.CustomerEmail,
+                    customerName = order.CustomerName
+                };
+
+                var json = JsonSerializer.Serialize(payload);
+
+                using var httpClient = new HttpClient();
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                await httpClient.PostAsync(logicAppUrl, content);
+
+                _logger.LogInformation($"Logic App triggered for Order {orderId}");
             }
             else
             {
